9ae410690381b75e4b3d56b425bdf10dad452c68
Nasir Ahmad <nasiryahm@gmail.com>
2019-04-27 09:25:46 +0100

    Adding conditional writing to synaptic input buffers.

--- Spike/Backend/CUDA/InlineDeviceFunctions.hpp
+++ Spike/Backend/CUDA/InlineDeviceFunctions.hpp
@@ -39,9 +39,13 @@ __device__ float my_conductance_spiking_injection_kernel(
     float reversal_value = synaptic_data->reversal_potentials_Vhat[syn_label];
     float synaptic_conductance_g = synaptic_data->neuron_wise_conductance_trace[syn_label + idx*synaptic_data->num_syn_labels];
     // Update the synaptic conductance
-    synaptic_conductance_g = decay_factor*synaptic_conductance_g + synaptic_data->neuron_inputs.circular_input_buffer[bufferloc + syn_label + idx*synaptic_data->num_syn_labels];
-    // Reset the conductance update
-    synaptic_data->neuron_inputs.circular_input_buffer[bufferloc + syn_label + idx*synaptic_data->num_syn_labels] = 0.0f;
+    synaptic_conductance_g *= decay_factor;
+    float conductance_increment = synaptic_data->neuron_inputs.circular_input_buffer[bufferloc + syn_label + idx*synaptic_data->num_syn_labels];
+    if (conductance_increment != 0.0f){
+      synaptic_conductance_g += conductance_increment;
+      // Reset the conductance update
+      synaptic_data->neuron_inputs.circular_input_buffer[bufferloc + syn_label + idx*synaptic_data->num_syn_labels] = 0.0f;
+    }
     total_current += synaptic_conductance_g*(reversal_value - current_membrane_voltage);
 
     synaptic_data->neuron_wise_conductance_trace[syn_label + idx*synaptic_data->num_syn_labels] = synaptic_conductance_g;
@@ -71,9 +75,12 @@ __device__ float my_current_spiking_injection_kernel(
       float synaptic_current = synaptic_data->neuron_wise_current_trace[total_number_of_neurons*syn_label + idx];
       // Update the synaptic conductance
       synaptic_current *= decay_factor;
-      synaptic_current += synaptic_data->neuron_inputs.circular_input_buffer[bufferloc + syn_label + idx*synaptic_data->num_syn_labels];
-      // Reset the conductance update
-      synaptic_data->neuron_inputs.circular_input_buffer[bufferloc + syn_label*total_number_of_neurons + idx] = 0.0f;
+      float current_inc = synaptic_data->neuron_inputs.circular_input_buffer[bufferloc + syn_label + idx*synaptic_data->num_syn_labels];
+      if (current_inc != 0.0){
+        synaptic_current += current_inc;
+        // Reset the conductance update
+        synaptic_data->neuron_inputs.circular_input_buffer[bufferloc + syn_label*total_number_of_neurons + idx] = 0.0f;
+      }
       total_current += synaptic_current;
       synaptic_data->neuron_wise_current_trace[total_number_of_neurons*syn_label + idx] = synaptic_current;
 
@@ -99,10 +106,11 @@ __device__ float my_voltage_spiking_injection_kernel(
 
   float total_current = 0.0f;
   for (int syn_label = 0; syn_label < synaptic_data->num_syn_labels; syn_label++){
-    total_current += synaptic_data->neuron_inputs.circular_input_buffer[bufferloc + syn_label + idx*synaptic_data->num_syn_labels];
-    
-    synaptic_data->neuron_inputs.circular_input_buffer[bufferloc + syn_label + idx*synaptic_data->num_syn_labels] = 0.0f;
-    
+    float input_current = synaptic_data->neuron_inputs.circular_input_buffer[bufferloc + syn_label + idx*synaptic_data->num_syn_labels];
+    if (input_current != 0.0f){
+      total_current += input_current;
+      synaptic_data->neuron_inputs.circular_input_buffer[bufferloc + syn_label + idx*synaptic_data->num_syn_labels] = 0.0f;
+    }
   }
 
 
