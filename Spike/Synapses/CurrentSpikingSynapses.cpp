1737fdf35829c00b73d56caa437636f507ff813b
Nasir Ahmad <nasiryahm@gmail.com>
2019-05-10 11:45:32 +0100

    Adding frontend code for Current based synapses.

--- Spike/Synapses/CurrentSpikingSynapses.cpp
+++ Spike/Synapses/CurrentSpikingSynapses.cpp
@@ -17,7 +17,27 @@ int CurrentSpikingSynapses::AddGroup(int presynaptic_group_id,
 
   current_spiking_synapse_parameters_struct * current_spiking_synapse_group_params = (current_spiking_synapse_parameters_struct*)synapse_params;
   
-  decay_terms_tau.push_back(current_spiking_synapse_group_params->decay_term_tau);
+  // Set up unique parameter labels
+  bool found = false;
+  int param_label = 0;
+  for (int p = 0; p < decay_terms_tau.size(); p++){
+    if (current_spiking_synapse_group_params->decay_term_tau == decay_terms_tau[p]){
+      found = true;
+      param_label = p;
+    }
+  }
+  if (!found){
+    param_label = decay_terms_tau.size();
+    // Set constants
+    decay_terms_tau.push_back(current_spiking_synapse_group_params->decay_term_tau);
+    // Keep number of parameter labels up to date
+    number_of_parameter_labels = param_label + 1;
+  }
+
+  for (int i = (total_number_of_synapses - temp_number_of_synapses_in_last_group); i < total_number_of_synapses; i++) {
+    parameter_labels[i] = param_label;
+  }
+  
   return(groupID);
 }
 
