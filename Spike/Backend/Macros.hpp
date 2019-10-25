e5d36741f3a1eaa4ac249c2cac4bd05620da269f
Nikolaos Kyriazis <nkyriazis@gmail.com>
2019-10-25 12:39:12 +0300

    Compiles on windows and seems to link well on the other side

--- Spike/Backend/Macros.hpp
+++ Spike/Backend/Macros.hpp
@@ -3,9 +3,40 @@
 #include <iostream>
 
 #ifndef NDEBUG
-#include <cxxabi.h>
+#ifndef _MSC_VER
+#   include <cxxabi.h>
+#endif
+
+template <class T>
+std::string
+type_name()
+{
+    typedef typename std::remove_reference<T>::type TR;
+    std::unique_ptr<char, void(*)(void*)> own
+    (
+#ifndef _MSC_VER
+        abi::__cxa_demangle(typeid(TR).name(), nullptr,
+            nullptr, nullptr),
+#else
+        nullptr,
+#endif
+        std::free
+    );
+    std::string r = own != nullptr ? own.get() : typeid(TR).name();
+    if (std::is_const<TR>::value)
+        r += " const";
+    if (std::is_volatile<TR>::value)
+        r += " volatile";
+    if (std::is_lvalue_reference<T>::value)
+        r += "&";
+    else if (std::is_rvalue_reference<T>::value)
+        r += "&&";
+    return r;
+}
+
 // From http://stackoverflow.com/questions/81870/is-it-possible-to-print-a-variables-type-in-standard-c#comment63837522_81870 :
-#define TYPEID_NAME(x) abi::__cxa_demangle(typeid((x)).name(), NULL, NULL, NULL)
+//#define TYPEID_NAME(x) abi::__cxa_demangle(typeid((x)).name(), NULL, NULL, NULL)
+#define TYPEID_NAME(x) type_name<decltype(x)>()
 #endif
 
 #define SPIKE_ADD_BACKEND_GETSET(TYPE, SUPER)           \
